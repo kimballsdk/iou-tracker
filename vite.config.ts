c6443bc73beb4e01fbfcec6ed2072e6b347fae90
Samuel Kimball <134667187+GamerobotVentura@users.noreply.github.com>
2025-12-17 21:49:16 -0700

    remove comment

--- vite.config.ts
+++ vite.config.ts
@@ -5,5 +5,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/iou-tracker/', // <-- IMPORTANT: Change 'iou-tracker' to your GitHub repository name
+  base: '/iou-tracker/',
 })
